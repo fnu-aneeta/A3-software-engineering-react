3edf02fb504189f7f6413879942a30bd8714001e
aneeta-f <aneeta.ochani@hotmail.com>
2022-03-20 01:22:14 -0400

    pushing a3

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 // const TUITS_API = "https://cs5500-01-sp22.herokuapp.com/api/tuits";
 const TUITS_API = "http://localhost:4000/api/tuits";
+// const TUITS_API = "https://cs-5500a2.herokuapp.com/api/tuits";
 // const USERS_API = "https://cs5500-01-sp22.herokuapp.com/api/users";
 const USERS_API = "http://localhost:4000/api/users";
+// const USERS_API = "https://cs-5500a2.herokuapp.com/api/users";
+
 
 export const findAllTuits = () =>
     axios.get(TUITS_API)
@@ -21,6 +24,7 @@ export const createTuit = (uid, tuit) =>
     axios.post(`${USERS_API}/${uid}/tuits`, tuit)
         .then(response => response.data);
 
+
 export const updateTuit = (tid, tuit) =>
     axios.post(`${TUITS_API}/${tid}`, tuit)
         .then(response => response.data);
@@ -28,3 +32,4 @@ export const updateTuit = (tid, tuit) =>
 export const deleteTuit = (tid) =>
     axios.delete(`${TUITS_API}/${tid}`)
         .then(response => response.data);
+
